889f82a9e2ccd96b93b884a89ce164bcdaa2237d
Edward Reed <edwardareed@gmail.com>
2015-12-27 13:53:33 -0800

    Refator backstagePass Method

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -79,41 +79,12 @@ end
 def sulfuras(item) end
 
 def backstagePass(item)
-
-  if item.quality == 50
-    item.quality -= 2
-  end
-
-  if item.sell_in == 6
-    item.quality += 1
-  end
-
-  if item.sell_in == 10
-    item.quality += 1
-  end
-
-  if item.sell_in < 0
-    item.quality = -1
-  end
-
-  if item.sell_in == 1
-    item.quality += 2
-  end
-
-  if item.sell_in == 5
-    item.quality += 2
-  end
-
-  if item.quality == 50
-    item.quality -= 1
-  end
-
-  if item.sell_in == 0
-    item.quality = -1
-  end
-
-  item.quality += 1
   item.sell_in -= 1
+  return if item.quality == 50
+  return item.quality = 0 if item.sell_in < 0
+  item.quality += 1
+  item.quality += 1 if item.sell_in < 10
+  item.quality += 1 if item.sell_in < 5
 end
 # DO NOT CHANGE THINGS BELOW -----------------------------------------
 
